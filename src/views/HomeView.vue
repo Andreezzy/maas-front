a63cf27abeab3e244ee814cab1f473c2e68f606a
Andreezzy <frandres333@gmail.com>
2022-11-05 22:24:27 -0500

    add styles for the Login View

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore();
+const { user } = storeToRefs(userStore);
+
+</script>
+
+<template>
+  <div>
+    <h1 className="text-3xl font-bold underline text-sky-400">
+      Hello world!
+    </h1>
+    <h1>Hi {{user?.firstName}}!</h1>
+    <p>You're logged in with Vue 3 + Pinia & JWT!!</p>
+  </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
+import { useUserStore } from '@/stores/user';
+
+import { LockClosedIcon } from '@heroicons/vue/20/solid'
+
+const schema = Yup.object().shape({
+  email: Yup.string().required('Email is required'),
+  password: Yup.string().required('Password is required')
+});
+
+function onSubmit(values, { setErrors }) {
+  const user = useUserStore();
+  const { email, password } = values;
+  return user.login(email, password)
+                  .catch(error => setErrors({ apiError: error }));
+}
+</script>
+
+<template>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+      <Form class="mt-8 space-y-6" @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <Field id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" :class="{ 'is-invalid': errors.email }" />
+            <div class="invalid-feedback">{{errors.email}}</div>
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <Field id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" :class="{ 'is-invalid': errors.password }" />
+            <div class="invalid-feedback">{{errors.password}}</div>
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+          </div>
+
+          <div class="text-sm">
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+          </div>
+        </div>
+
+        <div>
+          <button :disabled="isSubmitting" type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+            Sign in
+          </button>
+        </div>
+        <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{errors.apiError}}</div>
+      </Form>
+    </div>
+  </div>
+</template>
