4cb2f4985551d9a5536895e18ef23b91d51bf0dc
Andreezzy <frandres333@gmail.com>
2022-11-06 23:04:45 -0500

    add SelectMenu component to Dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-  import { useUserStore } from '@/stores/user';
-
   import NavBar from "@/components/layout/NavBar.vue"
-
-  const userStore = useUserStore();
-
   import EventCalendar from "@/components/EventCalendar.vue"
+  import SelectMenu from "@/components/SelectMenu.vue"
 
+  import { useCompanyStore } from '@/stores/company';
+  import { storeToRefs } from "pinia";
+
+  const companyStore = useCompanyStore();
+
+  const { all, setSelected } = companyStore
+  const { selected, schedules } = storeToRefs(companyStore)
+
+  const test = (val: object) => console.log(val)
 </script>
 
 <template>
@@ -14,13 +19,17 @@
     <NavBar />
     <main class="-mt-32">
       <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
         <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
-          <div class="h-96 rounded-lg border-4 border-dashed border-gray-200">
-            <EventCalendar />
+          <div class="h-full min-h-[100px] rounded-lg border-gray-200">
+            <div class="flex ">
+              <SelectMenu text-label='Company' :values="all" :onSelect="setSelected" class="mr-4" />
+              <SelectMenu text-label='Week' :onSelect="test" :values="schedules" />
+            </div>
+            <div class="mt-4">
+              <EventCalendar />
+            </div>
           </div>
         </div>
-        <!-- /End replace -->
       </div>
     </main>
   </div>
