787522a973a9fa6c525374e6f46e58e34f63c2c3
Andreezzy <frandres333@gmail.com>
2022-11-09 14:34:03 -0500

    add updateMyEvents to EventService

--- src/services/event.ts
+++ src/services/event.ts
@@ -10,4 +10,17 @@ export class EventService {
       throw error
     }
   }
+
+  public updateMyEvents = async (events: any, schedule_id: number): Promise<any> => {
+    try {
+      const payload = events.map(({start: start_time, end: end_time, ...vl})=>({start_time, end_time, ...vl}));
+      const response = await api.post(`events?schedule_id=${schedule_id}`, {
+        events: payload
+      })
+      return <any>response.data
+    } catch (error) {
+      console.error("service EventService#getAll  ", error)
+      throw error
+    }
+  }
 }
