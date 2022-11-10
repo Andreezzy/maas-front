e545ee0814b8c5e4c2d853d96c95dbfb2b4a20ee
Andreezzy <frandres333@gmail.com>
2022-11-10 10:50:27 -0500

    fix warnings

--- src/stores/event.ts
+++ src/stores/event.ts
@@ -21,56 +21,10 @@ interface State {
 export const useEventStore = defineStore('event', {
 
   state: (): State => ({
-    myDrafts: [
-      {
-        id: '1',
-        title: 'Andres',
-        start: '2022-11-11T10:00:00',
-        end: '2022-11-11T12:00:00',
-        backgroundColor: '#ff0000',
-        borderColor: '#ff0000',
-        extendedProps: {
-          schedule_id: 1
-        },
-      },
-    ],
-    allDrafts: [
-      {
-        id: '1',
-        title: 'Andres',
-        start: '2022-11-11T10:00:00',
-        end: '2022-11-11T12:00:00',
-        backgroundColor: '#ff0000',
-        borderColor: '#ff0000',
-        extendedProps: {
-          schedule_id: 1
-        },
-      },
-      {
-        id: '2',
-        title: 'Jorge',
-        start: '2022-11-11T10:00:00',
-        end: '2022-11-11T12:00:00',
-        backgroundColor: '#ff0000',
-        borderColor: '#ff0000',
-        extendedProps: {
-          schedule_id: 1
-        },
-      },
-      {
-        id: '3',
-        title: 'Barbara',
-        start: '2022-11-10T10:00:00',
-        end: '2022-10-12T13:00:00',
-        backgroundColor: '#ff0000',
-        borderColor: '#ff0000',
-        extendedProps: {
-          schedule_id: 1
-        },
-      }
-    ],
-    allPublished: [],
+    myDrafts: [],
+    allDrafts: [],
     activeEvents: [],
+    allPublished: [],
     title: '',
   }),
 
